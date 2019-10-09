77755f828448b2e0de375b5d3a9e3d66d5b09bfe
Colton McCurdy <mccurdyc22@gmail.com>
2019-10-09 08:41:08 -0400

    fixed fzf cli keybindings
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,5 +1,6 @@
 # Setup fzf
-# ---------
+# one "gotcha" is that brew installs to /usr/local/bin/fzf and aur installs to /usr/share/fzf
+
 if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/usr/share/fzf/bin"
 fi
@@ -9,5 +10,10 @@ fi
 [[ $- == *i* ]] && source "/usr/share/fzf/completion.zsh" 2> /dev/null
 
 # Key bindings
+# https://github.com/junegunn/fzf#key-bindings-for-command-line
 # ------------
 source "/usr/share/fzf/key-bindings.zsh"
+
+# follow symbolic links, and don't want it to exclude hidden files
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
