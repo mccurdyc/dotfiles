19c04f2b12136f7ca0cbd86404d0e9da5076aef7
Colton McCurdy <mccurdyc22@gmail.com>
2019-12-17 08:23:11 -0500

    fzf config
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,19 +1,13 @@
 # Setup fzf
-# one "gotcha" is that brew installs to /usr/local/bin/fzf and aur installs to /usr/share/fzf
-
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/share/fzf/bin"
+# ---------
+if [[ ! "$PATH" == */home/mccurdyc/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/mccurdyc/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/share/fzf/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/mccurdyc/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
-# https://github.com/junegunn/fzf#key-bindings-for-command-line
 # ------------
-source "/usr/share/fzf/key-bindings.zsh"
-
-# follow symbolic links, and don't want it to exclude hidden files
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+source "/home/mccurdyc/.fzf/shell/key-bindings.zsh"
