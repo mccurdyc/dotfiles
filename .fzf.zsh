f16e0cae3648111b61aee127fd9df3c239d7797c
Colton McCurdy <mccurdyc22@gmail.com>
2019-12-23 09:54:04 -0500

    per-os fzf config
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,19 +1,22 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/mccurdyc/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/mccurdyc/.fzf/bin"
-fi
+#
+if [[ $OSTYPE == *"darwin"* ]]; then
+  if [[ ! "$PATH" == */Users/mccurdyc/.fzf/bin* ]]; then
+    export PATH="${PATH:+${PATH}:}/Users/mccurdyc/.fzf/bin"
+  fi
 
-if [[ ! "$PATH" == */Users/mccurdyc/.fzf/shell* ]]; then
-  [[ $- == *i* ]] && source "/Users/mccurdyc/.fzf/shell/completion.zsh" 2> /dev/null
-  source "/Users/mccurdyc/.fzf/shell/key-bindings.zsh"
-fi
+  if [[ ! "$PATH" == */Users/mccurdyc/.fzf/shell* ]]; then
+    [[ $- == *i* ]] && source "/Users/mccurdyc/.fzf/shell/completion.zsh" 2> /dev/null
+    source "/Users/mccurdyc/.fzf/shell/key-bindings.zsh"
+  fi
+elif [[ $OSTYPE == *"linux"* ]]; then
+  if [[ ! "$PATH" == */home/mccurdyc/.fzf/bin* ]]; then
+    export PATH="${PATH:+${PATH}:}/home/mccurdyc/.fzf/bin"
+  fi
 
-if [[ ! "$PATH" == */home/mccurdyc/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/mccurdyc/.fzf/bin"
-fi
-
-if [[ ! "$PATH" == */home/mccurdyc/.fzf/shell* ]]; then
-  [[ $- == *i* ]] && source "/home/mccurdyc/.fzf/shell/completion.zsh" 2> /dev/null
-  source "/home/mccurdyc/.fzf/shell/key-bindings.zsh"
+  if [[ ! "$PATH" == */home/mccurdyc/.fzf/shell* ]]; then
+    [[ $- == *i* ]] && source "/home/mccurdyc/.fzf/shell/completion.zsh" 2> /dev/null
+    source "/home/mccurdyc/.fzf/shell/key-bindings.zsh"
+  fi
 fi
