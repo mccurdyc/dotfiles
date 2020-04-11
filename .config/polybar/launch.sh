dc66e041d3a278262de9fc3fc5c38749ec9e2fa1
Colton McCurdy <cmccurdy@fastly.com>
2020-04-11 11:11:32 -0400

    polybar-launch: fix launch script (was broken)

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# https://github.com/polybar/polybar/issues/763
+# I couldn't use 'polybar --list-monitors'.
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload bottom &
