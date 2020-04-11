981c8af91452e9862b18166b61ba9770b668666c
Colton McCurdy <cmccurdy@fastly.com>
2020-04-11 06:18:14 -0400

    fzf.zsh: just add the keybindings

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,22 +1,13 @@
 # Setup fzf
 # ---------
-#
-if [[ $OSTYPE == *"darwin"* ]]; then
-  if [[ ! "$PATH" == */Users/mccurdyc/.fzf/bin* ]]; then
-    export PATH="${PATH:+${PATH}:}/Users/mccurdyc/.fzf/bin"
-  fi
-
-  if [[ ! "$PATH" == */Users/mccurdyc/.fzf/shell* ]]; then
-    [[ $- == *i* ]] && source "/Users/mccurdyc/.fzf/shell/completion.zsh" 2> /dev/null
-    source "/Users/mccurdyc/.fzf/shell/key-bindings.zsh"
-  fi
-elif [[ $OSTYPE == *"linux"* ]]; then
-  if [[ ! "$PATH" == */home/mccurdyc/.fzf/bin* ]]; then
-    export PATH="${PATH:+${PATH}:}/home/mccurdyc/.fzf/bin"
-  fi
-
-  if [[ ! "$PATH" == */home/mccurdyc/.fzf/shell* ]]; then
-    [[ $- == *i* ]] && source "/home/mccurdyc/.fzf/shell/completion.zsh" 2> /dev/null
-    source "/home/mccurdyc/.fzf/shell/key-bindings.zsh"
-  fi
+if [[ ! "$PATH" == */home/mccurdyc/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/mccurdyc/.fzf/bin"
 fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/mccurdyc/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/mccurdyc/.fzf/shell/key-bindings.zsh"
