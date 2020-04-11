6e699c0ca8b08b7f55038578fcc9eaf388519bb4
Colton McCurdy <cmccurdy@fastly.com>
2020-04-11 06:23:10 -0400

    polybar all monitors. kind of ugly
    Signed-off-by: Colton McCurdy <cmccurdy@fastly.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,4 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-exec polybar --reload main
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bottom &
+  done
+else
+  polybar --reload bottom &
+fi
