ff678dcc9cbcf668d707d337be49143a0729a847
Colton McCurdy <cmccurdy@fastly.com>
2020-05-15 15:47:34 -0400

    top and bottom polybars
    Signed-off-by: Colton McCurdy <cmccurdy@fastly.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,12 +6,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+POLYBAR_CONFIG=$XDG_CONFIG_HOME/polybar/config
+
 # https://github.com/polybar/polybar/issues/763
 # I couldn't use 'polybar --list-monitors'.
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload bottom &
+    # https://github.com/adi1090x/polybar-themes/blob/master/polybar-5/launch.sh
+    MONITOR=$m polybar -c $POLYBAR_CONFIG top &
+    MONITOR=$m polybar -c $POLYBAR_CONFIG bottom &
   done
 else
-  polybar --reload bottom &
+  polybar -c $POLYBAR_CONFIG top &
+  polybar -c $POLYBAR_CONFIG bottom &
 fi
