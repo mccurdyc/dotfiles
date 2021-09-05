0dee70f69d76574a1adbd0b11f8afb1beb05ab13
Colton J. McCurdy <mccurdyc22@gmail.com>
2021-09-05 10:22:46 -0400

    organizing plugins
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- files.headless/.config/nvim/lua/plugins.lua
+++ files.headless/.config/nvim/lua/plugins.lua
@@ -17,13 +17,26 @@ return require('packer').startup({function()
   -- https://github.com/wbthomason/packer.nvim/blob/daec6c759f95cd8528e5dd7c214b18b4cec2658c/doc/packer.txt#L534
 
   use 'wbthomason/packer.nvim'
+
+  -- NeoVim LSP
   use 'neovim/nvim-lspconfig'
   use 'nvim-lua/completion-nvim'
   use 'nvim-lua/diagnostic-nvim'
+
+  -- Linting
+  use {
+    'w0rp/ale',
+    ft = { 'sh', 'zsh', 'bash', 'html', 'markdown', 'racket', 'vim', 'go', 'rs', 'tf' },
+    cmd = 'ALEEnable',
+    config = 'vim.cmd[[ALEEnable]]'
+  }
+
+  -- Git
   use 'tpope/vim-fugitive'
   use 'jreybert/vimagit'
-  use 'tpope/vim-surround'
-  use 'airblade/vim-gitgutter'
+  use 'ruanyl/vim-gh-line'
+
+  -- Go
   use { 'fatih/vim-go',
       ft = 'go',
       run = ':GoUpdateBinaries',
@@ -31,28 +44,32 @@ return require('packer').startup({function()
   use { 'sebdah/vim-delve',
       ft ='go',
   }
+
+  -- Rust
   use { 'rust-lang/rust.vim',
       ft = 'rs',
   }
-  use 'hashivim/vim-terraform'
+
+  -- Terraform
+  use { 'hashivim/vim-terraform',
+    ft = { 'tf', 'hcl' }
+  }
+
+  -- General
+  use 'tpope/vim-surround'
+  use 'airblade/vim-gitgutter'
+  use 'SirVer/ultisnips'
+  use 'mccurdyc/vim-snippets'
+  use 'airblade/vim-rooter'
+  use 'tomtom/tcomment_vim'
+  use { 'junegunn/fzf', run = './install --bin', }
+
+  -- Themes / Colors
   use 'edkolev/tmuxline.vim'
   use 'vim-scripts/colorizer'
   use 'itchyny/lightline.vim'
   use 'mccurdyc/vim-base16-lightline'
   use 'mccurdyc/base16-vim'
-  use 'SirVer/ultisnips'
-  use 'mccurdyc/vim-snippets'
-  use { 'junegunn/fzf', run = './install --bin', }
-
-  use {
-    'w0rp/ale',
-    ft = { 'sh', 'zsh', 'bash', 'html', 'markdown', 'racket', 'vim', 'go', 'rs', 'tf' },
-    cmd = 'ALEEnable',
-    config = 'vim.cmd[[ALEEnable]]'
-  }
-  use 'airblade/vim-rooter'
-  use 'tomtom/tcomment_vim'
-  use 'ruanyl/vim-gh-line'
 end,
 -- Use a floating window
 -- https://github.com/wbthomason/packer.nvim#using-a-floating-window
