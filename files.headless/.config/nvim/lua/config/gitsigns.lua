7c1d7914cd8b5cca6792e36c34e223c09ea7066d
Colton J. McCurdy <mccurdyc22@gmail.com>
2021-09-06 12:15:56 -0400

    [nvim] show git blame inline
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- files.headless/.config/nvim/lua/config/gitsigns.lua
+++ files.headless/.config/nvim/lua/config/gitsigns.lua
@@ -37,11 +37,11 @@ require('gitsigns').setup {
     follow_files = true
   },
   attach_to_untracked = true,
-  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
-    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
+    virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+    delay = 2000,
   },
   current_line_blame_formatter_opts = {
     relative_time = false
