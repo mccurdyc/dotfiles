0a53b6211f9b057a2753a5644b154a2661b974c3
Colton J. McCurdy <mccurdyc22@gmail.com>
2021-09-05 23:56:55 -0400

    [nvim] pretty statusline
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- files.headless/.config/nvim/lua/config/statusline.lua
+++ files.headless/.config/nvim/lua/config/statusline.lua
@@ -1,1 +1,36 @@
-require('lualine').setup()
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'modus_vivendi',
+    component_separators = '',
+    section_separators = '',
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch',
+    { 'diff',
+      colored = true,
+      color_added = '#66cccc',
+      color_modified = '#ffcc66',
+      color_removed = '#f2777a',
+      symbols = {added = '+', modified = '~', removed = '-'},
+    }
+  },
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {'fzf', 'quickfix'}
+}
+
