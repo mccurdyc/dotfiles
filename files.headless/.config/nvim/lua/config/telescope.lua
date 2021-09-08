168619147053a8eae9c27f8a751db1642c2fa4e5
Colton J. McCurdy <mccurdyc22@gmail.com>
2021-09-08 12:26:11 -0400

    [nvim] telescope absolute full path
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- files.headless/.config/nvim/lua/config/telescope.lua
+++ files.headless/.config/nvim/lua/config/telescope.lua
@@ -36,9 +36,7 @@ require("telescope").setup {
     file_sorter = require "telescope.sorters".get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
-    path_display = {
-      shorten = 1
-    },
+    path_display = absolute,
     winblend = 0,
     border = {},
     borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
