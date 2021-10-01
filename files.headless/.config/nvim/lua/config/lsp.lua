1f5ae656ecb968a6db4cbacc2930aedf49f4bb8a
Colton J. McCurdy <mccurdyc22@gmail.com>
2021-10-01 09:16:51 -0400

    [nvim] fix accidentally overwrote grep keybinding
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- files.headless/.config/nvim/lua/config/lsp.lua
+++ files.headless/.config/nvim/lua/config/lsp.lua
@@ -16,13 +16,9 @@ map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 map("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
 map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-map("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
 map("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
 map("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-map("n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-map("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
 local g = vim.g
 
