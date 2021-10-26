ee21c8e705ea81b7c2ac23eb74216baff3546a5d
Colton J. McCurdy <mccurdyc22@gmail.com>
2021-10-25 20:57:44 -0400

    [nvim] don't show snippet in quickfix

--- files.headless/.config/nvim/lua/config/quickfix.lua
+++ files.headless/.config/nvim/lua/config/quickfix.lua
@@ -1,11 +1,9 @@
 require("bqf").setup(
   {
     auto_enable = true,
+    magic_window = false,
     preview = {
-      win_height = 12,
-      win_vheight = 12,
-      delay_syntax = 80,
-      border_chars = {"┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█"}
+      auto_preview = false
     },
     func_map = {
       vsplit = "",
