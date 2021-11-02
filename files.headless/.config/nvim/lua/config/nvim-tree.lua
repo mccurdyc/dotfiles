37f17a30bd8f6da6b95868d6c656bbf2c90d4e92
Colton J. McCurdy <mccurdyc22@gmail.com>
2021-11-01 20:08:59 -0400

    fix nvim-tree error temporarily
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- files.headless/.config/nvim/lua/config/nvim-tree.lua
+++ files.headless/.config/nvim/lua/config/nvim-tree.lua
@@ -3,7 +3,7 @@ local g = vim.g
 -- Open NvimTree on Vim open.
 -- vim.cmd [[autocmd VimEnter * NvimTreeOpen]]
 
-g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
+-- g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
 g.nvim_tree_window_picker_exclude = {
   filetype = {
     "notify",
