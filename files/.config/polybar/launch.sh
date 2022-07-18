3a84695f67953b12ed68044ca43ff685fef5928e
Colton J. McCurdy <mccurdyc22@gmail.com>
2022-07-17 20:49:47 -0400

    [polybar] update launch script
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- files/.config/polybar/launch.sh
+++ files/.config/polybar/launch.sh
@@ -4,19 +4,19 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-POLYBAR_CONFIG=$XDG_CONFIG_HOME/polybar/config.ini
+POLYBAR_CONFIG="$XDG_CONFIG_HOME/polybar/config.ini"
 
 # https://github.com/polybar/polybar/issues/763
 # I couldn't use 'polybar --list-monitors'.
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    # https://github.com/adi1090x/polybar-themes/blob/master/polybar-5/launch.sh
-    MONITOR=$m polybar -c $POLYBAR_CONFIG top &
-    MONITOR=$m polybar -c $POLYBAR_CONFIG bottom &
-  done
-else
-  polybar -c $POLYBAR_CONFIG top &
-  polybar -c $POLYBAR_CONFIG bottom &
-fi
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     # https://github.com/adi1090x/polybar-themes/blob/master/polybar-5/launch.sh
+#     MONITOR=$m polybar -c $POLYBAR_CONFIG top &
+#     MONITOR=$m polybar -c $POLYBAR_CONFIG bottom &
+#   done
+# else
+polybar -c "$POLYBAR_CONFIG" top &
+polybar -c "$POLYBAR_CONFIG" bottom &
+# fi
